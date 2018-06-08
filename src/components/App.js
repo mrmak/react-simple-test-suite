4ba34624b7d139d7fbcef7bc314d9ab2f1548af6
Andrew Macnider <amacnider@gmail.com>
2018-06-08 16:20:53 -0700

    Simple testing dashboard

---
+++ src/components/App.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import TestPanel from './TestPanel';
+import '../styles/App.css';
+
+function generateTest(baseDelay = 5000) {
+	return function runTest() {
+		const delay = baseDelay + (Math.random() * baseDelay);
+		const testPassed = Math.random() > 0.5;
+
+		return new Promise(function(resolve) {
+			setTimeout(function() {
+				resolve(testPassed);
+			}, delay);
+		});
+	};
+}
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			tests: [
+				{description: 'System Status',  id: 'unique_key1', didPass: null, count: 0, numSuccess: 0, run: generateTest()},
+				{description: 'Server1 Status', id: 'unique_key2', didPass: null, count: 0, numSuccess: 0, run: generateTest()},
+				{description: 'Server2 Status', id: 'unique_key3', didPass: null, count: 0, numSuccess: 0, run: generateTest()},
+				{description: 'Server3 Status', id: 'unique_key4', didPass: null, count: 0, numSuccess: 0, run: generateTest()},
+				{description: 'Server4 Status', id: 'unique_key5', didPass: null, count: 0, numSuccess: 0, run: generateTest()},
+				{description: 'Server5 Status', id: 'unique_key6', didPass: null, count: 0, numSuccess: 0, run: generateTest(),},
+			],
+		};
+	}
+	runAllTests(){
+		// Run any test that is currently not running
+		const runTestButtons = document.getElementsByClassName('test-button');
+
+		for (let i = 0; i < runTestButtons.length; i++){
+			if (!runTestButtons[i].disabled){
+				runTestButtons[i].click();
+			}
+		}
+	}
+	handleClick(e, id){
+		const testIndex = this.state.tests.findIndex(obj => obj.id === id);
+		this.runTest(this.state.tests[testIndex], id, e.target);
+	}
+	runTest(obj, id, button){
+		const { run } = obj;
+		let { didPass, count, numSuccess } = obj;
+
+		this.toggleTestButtonStatus(button);
+		run().then((response) => {
+			count += 1;
+			didPass = response;
+			if (response) {
+				numSuccess += 1;
+			}
+			obj.id = id;
+			obj.count = count;
+			obj.didPass = didPass;
+			obj.numSuccess = numSuccess;
+			this.setState({ obj } );
+			this.toggleTestButtonStatus(button);
+		});
+	}
+	toggleTestButtonStatus(button){
+		button.disabled = button.disabled ? false : true;
+		button.innerHTML = button.innerHTML === 'Run' ? 'Running...' : 'Run';
+	}
+	render() {
+		const testPanels = [];
+		const tests = this.state.tests;
+		for (let i = 0; i < tests.length; i += 1) {
+			const { description, id, didPass, count, numSuccess } = tests[i];
+			testPanels.push(<TestPanel 
+				key={id}
+				id={id}
+				description={description}
+				didPass={didPass}
+				count={count}
+				numSuccess={numSuccess}
+				onClick={e => { this.handleClick(e, id); }}
+			/>);
+		}
+		
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">System Test Dashboard</h1>
+					<button onClick={() => { this.runAllTests(); }} >Run All Tests</button>
+				</header>
+				<div className="test-container">
+					{ testPanels }
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/TestPanel.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/TestPanel.css';
+
+const TestPanel = props => (
+	<div className="test-panel">
+		<p className="title">{props.description}</p>
+		<p>Result: { props.didPass === null ? 'Awaiting test...' : props.didPass ? 'Passed' : 'Failed' }</p>
+		<p>Tests Completed: { props.count }</p>
+		<p>Tests Passed: { props.numSuccess }</p>
+		<p>Success Rate: { isNaN(props.numSuccess/props.count) ? 0 : (props.numSuccess/props.count * 100).toFixed(2) }%</p>
+		<button className="test-button" onClick={ (e) => { props.onClick(e, props.id); }}>Run</button>
+	</div>
+);
+
+TestPanel.propTypes = {
+	description: PropTypes.string.isRequired,
+	onClick: PropTypes.func.isRequired
+};
+
+export default TestPanel;
